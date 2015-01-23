e214d6027e8df70e4209b92e1dcf9e7023f2be5c
Amanda Luhrs <amanda.luhrs@gmail.com>
2015-01-22 20:11:15 -0500

    added simple post list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
+import random
 from django.shortcuts import render
+from .models import Post
+
+#python manage.py shell > may have to install ipython
 
 # Create your views here.
+def post_list(request): # simple view, accepts requests
+
+	# post = Post.objects.all() # REtrieve all posts even unpublished ones
+	posts = Post.objects.filter(published_date__isnull=False)
+	# post = [post for post in posts]
+
+	return render(request, 'blog/post_list.html', {
+			'post' : posts
+	}) #blog/post template
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)), #python package - no quotes
+	url(r'', include('blog.urls')),
+)
+
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+		
